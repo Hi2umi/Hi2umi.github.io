f201dd1291e880ec351a2dec893ea8ecb90cc87f
Hi2umi <91732721+Hi2umi@users.noreply.github.com>
2021-10-01 20:49:50 +0800

    Add files via upload

---
+++ js/time.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+function disptime(){
+    var today=new Date();
+    var hh=today.getHours();
+    var mm=today.getMinutes();
+    var ss=today.getSeconds();
+    document.getElementById("clock").innerHTML="现在的时间是："+hh+":"+mm+":"+ss;
+}
+setInterval("disptime()",1000);
