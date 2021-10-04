6041f1cd71ddbb1ac27b00ed633ecba2f797292f
Hi2umi <91732721+Hi2umi@users.noreply.github.com>
2021-10-04 14:54:48 +0800

    Update time.js

--- js/time.js
+++ js/time.js
@@ -5,6 +5,5 @@ function disptime(){
     var mm=today.getMinutes();
     var ss=today.getSeconds();
     document.getElementById("clock").innerHTML="现在的时间："+hh+":"+mm+":"+ss;
-    document.write("<h1>中文</h1>");
 }
 setInterval("disptime()",1000);
